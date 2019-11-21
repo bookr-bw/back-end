46da496eb94cca128e12a4773e2a44b8230fc626
RichardEsquivel <rlesquivel@gmail.com>
2019-11-20 18:28:08 -0600

    Modified logic within the findBy function within review-model.js in order to get the username returned as part of the request to reviewRouter.get('/:id')

--- reviews/review-model.js
+++ reviews/review-model.js
@@ -1,44 +1,47 @@
-const express = require("express");
+const express = require("express");
 
-const db = require("../data/dbConfig");
+const db = require("../data/dbConfig");
 
-module.exports = {
-  add,
-  remove,
-  update,
-  find,
-  findBy,
-  findById,
+module.exports = {
+	add,
+	remove,
+	update,
+	find,
+	findBy,
+	findById,
 };
 
-async function add(review) {
-  const [id] = await db("reviews").returning('id').insert(review, 'id');
+async function add(review) {
+	const [id] = await db("reviews").insert(review, 'id');
 
-  return findById(id);
+	return findById(id);
 }
 
-function remove(id) {
-  return db("reviews")
-    .where("id", id)
-    .delete();
+function remove(id) {
+	return db("reviews")
+		.where("id", id)
+		.delete();
 }
 
-async function update(review) {
-  return db("reviews")
-    .where({ id: review.id })
-    .update(review);
+async function update(review) {
+	return db("reviews")
+		.where({ id: review.id })
+		.update(review);
 }
 
-function find() {
-  return db("reviews");
+function find() {
+	return db("reviews");
 }
 
-function findBy(filter) {
-  return db("reviews").where(filter);
+function findBy(filter) {
+	return db("reviews")
+		.join('users', 'users.id', 'reviews.user_id')
+		.select('reviews.id', 'reviews.rating', 'reviews.review', 'reviews.books_id', 'reviews.user_id', 'users.username')
+		.where(filter)
 }
 
-function findById(id) {
-  return db("reviews")
-    .where({ id })
-    .first();
+function findById(id) {
+	return db("reviews")
+		.where({ id })
+		.first();
 }
--- reviews/review-router.js
+++ reviews/review-router.js
@@ -6,50 +6,48 @@ const { validateReview } = require("./review-helpers");
 const user = require("../users/users-model");
 
 reviewRouter.get("/:id", (req, res) => {
-  const { id } = req.params;
-
-  Review.findBy({ books_id: id })
-    .then(rev => {
-      if (rev) {
-        res.json(rev);
-      } else {
-        res.status(404).json({ message: "Could not find review." });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ message: "Failed to get review: " + err });
-    });
+	const { id } = req.params;
+	Review.findBy({ books_id: id })
+		.then(rev => {
+			if (rev) {
+				res.json(rev);
+			} else {
+				res.status(404).json({ message: "Could not find review." });
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ message: "Failed to get review: " + err });
+		});
 });
 
 reviewRouter.post("/:id", (req, res) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  const reviewData = req.body;
+	const reviewData = req.body;
 
-  const isValid = validateReview(reviewData);
+	const isValid = validateReview(reviewData);
 
-  if (isValid.isSuccessful) {
-    reviewData.books_id = id;
+	if (isValid.isSuccessful) {
+		reviewData.books_id = id;
 
-    user
-      .findBy({ username: req.decodedJwt.username })
-      .first()
-      .then(currentUser => {
-        reviewData.user_id = currentUser.id;
-
-        Review.add(reviewData)
-          .then(review => {
-            res.status(201).json(review);
-          })
-          .catch(err => {
-            res.status(500).json({ message: "Failed to create new review" });
-          });
-      });
-  } else {
-    res.status(500).json({
-      message: [isValid.errors]
-    });
-  }
+		user
+			.findBy({ username: req.decodedJwt.username })
+			.first()
+			.then(currentUser => {
+				reviewData.user_id = currentUser.id;
+				Review.add(reviewData)
+					.then(review => {
+						res.status(201).json(review);
+					})
+					.catch(err => {
+						res.status(500).json({ message: "Failed to create new review" });
+					});
+			});
+	} else {
+		res.status(500).json({
+			message: [isValid.errors]
+		});
+	}
 });
 
 module.exports = reviewRouter;
